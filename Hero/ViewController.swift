2c2c394a5b1b15a42c53c65af8c8a4010b33ea3c
Fang Shao <shao.fa@northeastern.edu>
2021-12-16 09:12:23 -0800

    Initial Commit

---
+++ Hero/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hero
+//
+//  Created by Fang Shao on 12/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
