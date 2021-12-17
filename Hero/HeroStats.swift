383eb6280620f490f95806d46d0eec2e830ad3bb
Fang Shao <shao.fa@northeastern.edu>
2021-12-16 18:59:26 -0800

    final

---
+++ Hero/HeroStats.swift
@@ -0,0 +1,17 @@
+//
+//  HeroStats.swift
+//  Hero
+//
+//  Created by Fang Shao on 12/16/21.
+//
+
+import Foundation
+struct HeroStats: Decodable{
+    let localized_name:String
+    let primary_attr: String
+    let attack_type: String
+    let legs: Int
+    let img: String
+    
+}
+
---
+++ Hero/HeroViewController.swift
@@ -0,0 +1,59 @@
+//
+//  HeroViewController.swift
+//  Hero
+//
+//  Created by Fang Shao on 12/16/21.
+//
+
+import UIKit
+
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
+
+class HeroViewController: UIViewController {
+    
+    
+    @IBOutlet weak var legLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var attributeLbl: UILabel!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    var hero:HeroStats?
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameLbl.text = hero?.localized_name
+        attributeLbl.text = hero?.primary_attr
+        attackLbl.text = hero?.attack_type
+        legLbl.text = "\((hero?.legs)!)"
+        let urlString = "https://api.opendota.com"+(hero?.img)!
+        let url = URL(string:urlString)!
+        imageView.downloaded(from: url)
+    }
+    
+    
+    
+    
+
+
+}
